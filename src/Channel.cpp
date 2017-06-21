6843a9c5279e8e6152fd8db7a2e7cf4f851efba4
Virgile Robles <virgile.robles@protonmail.ch>
2017-06-21 23:36:10 +0900

    Final fix

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 Channel::Channel(json::value v) :
 	id(v["id"].as_string()),
-	guild_id(v["guild_id"].as_string()),
 	name(v["name"].as_string()),
 	type(v["type"].as_string()),
 	position(v["position"].as_integer())
 {
+	if(v.has_field("guild_id"))
+		guild_id = v["guild_id"].as_string();
 	if(v.has_field("is_private"))
 		is_private = v["is_private"].as_bool();
 	if(v.has_field("last_message_id") and !v["last_message_id"].is_null())
