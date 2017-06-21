3e04fcfaaa17dc09aeb00c23eb895feba0183608
Virgile Robles <virgile.robles@protonmail.ch>
2017-06-21 23:25:07 +0900

    Fix 2

--- src/Guild.cpp
+++ src/Guild.cpp
@@ -10,7 +10,6 @@ Guild::Guild(json::value v) :
 	region(v["region"].as_string()),
 	afk_channel_id(v["afk_channel_id"].as_string()),
 	afk_timeout(v["afk_timeout"].as_integer()),
-	embed_enabled(v["embed_enabled"].as_bool()),
 	embed_channel_id(v["embed_channel_id"].as_string()),
 	verification_level(v["verification_level"].as_integer()),
 	default_message_notifications(v["default_message_notifications"]
@@ -18,6 +17,8 @@ Guild::Guild(json::value v) :
 {
 	if(v.has_field("splash") and !v["splash"].is_null())
 		splash = v["splash"].as_string();
+	if(v.has_field("embed_enabled") and !v["embed_enabled"].is_null())
+		embed_enabled = v["embed_enabled"].as_bool();
 	for(json::value& j : v["roles"].as_array())
 		roles.emplace_back(j);
 	if(v.has_field("mfa_level"))
